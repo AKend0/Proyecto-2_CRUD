4d25176655490cd60b449b9f4711b06b190c7b2a
Akend0 <theaoxd117@gmail.com>
2022-06-12 19:25:50 -0500

    segundo_avance_mostrar_pedidos

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const camppedido=document.getElementById('pedido');
 const campcliente=document.getElementById('cliente');
 const camptelefono=document.getElementById('telefono');
 const formularioContacto=document.getElementById('form');
+const cuerpotabla=document.getElementById('cuerpotabla');
+const lista=[];
+
+
+
 const guardarLocalStorage = () => {
     
 };
@@ -9,28 +14,57 @@ const recuperarLocalStorage = () => {};
 
 
 //! CREATE
-const guardarContacto = () => {
-    console.log();
+const guardarlista = (datoscontacto) => {
+    lista.push(datoscontacto);
+    mostrarlista();
+   // validamos si todo esta ok 
+   // console.log(lista);
+   
 };
 //!READ
-const mostrarContactos = () => {};
+const mostrarlista = () => {
+console.log(lista);
+    cuerpotabla.innerHTML=lista.reduce((acc,listas,index)=>{
+return acc+`
+<tr>
+      <th scope="row">${index+1}</th>
+      <td>${listas.Pedido}</td>
+      <td>${listas.Cliente}</td>
+      <td>${listas.Telefono}</td>
+      <td>
+      <button type="submit" class="btn btn-warning"onclick="editarlista(${index})">editar</button>
+      <button type="submit" class="btn btn-danger"onclick="eliminarlista(${index})">eliminar</button>
+      </td>
+    </tr>
+`
+
+    },'')
+    
+    
+};
 
 //! UPDATE
-const editarContacto = () => {};
+const editarlista = () => {};
 
 //! DELETE
-const eliminarContacto = () => {};
+const eliminarlista = () => {};
+
+//parte donde se colocara los datos para luego iniciar con el CRUD
 
 formularioContacto.addEventListener('submit', (event) => {
+
+    // ojo: cuando los campos estannvacios ,aparecera un alert diciendo que completes los datos
+    event.preventDefault();
+    
     if (!camppedido.value||!campcliente.value||!camptelefono.value) {
       alert('Esos datos no pueden estar vacios')
       return;
     }
-    event.preventDefault();
-    console.log({
-        pedido:camppedido.value,
-        cliente: campcliente.value,
-        telefono: camptelefono.value,
+    
+    guardarlista({
+        Pedido:camppedido.value,
+        Cliente: campcliente.value,
+        Telefono: camptelefono.value,
 
     });
     formularioContacto.reset();
